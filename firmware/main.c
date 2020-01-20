39746e26523266237f3c1b1f9ed4bcea2df4b5a9
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2020-01-20 20:51:54 +0100

    added firmware folder with start project

---
+++ firmware/main.c
@@ -0,0 +1,27 @@
+/* Name: main.c
+ * Projekt: sega_sound
+ * Author: Thorsten Kattanek
+ * Geändert am: 20.01.2020
+ * Copyright: Thorsten Kattanek
+ * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
+ *
+ */
+
+#define F_CPU   8000000UL
+    
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>   /* benötigt von usbdrv.h */
+
+// PinOut
+
+int main(void)
+{
+    while(1)
+    {
+    }
+}
+
+/* ------------------------------------------------------------------------- */
