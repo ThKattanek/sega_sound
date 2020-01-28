666c400e0cf2e5ca1dd7126d4bc1f648e1ee89e6
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2020-01-28 19:13:01 +0100

    changed simulation time in firmware: start_simulations.sh

--- firmware/start_simulation.sh
+++ firmware/start_simulation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make -j8
 simulavr -d atmega8 -o sega_sound.trc
-simulavr -d atmega8 -c vcd:sega_sound.trc:sega_sound.vcd -f sega_sound.elf -m 10000000 -F 8000000
+simulavr -d atmega8 -c vcd:sega_sound.trc:sega_sound.vcd -f sega_sound.elf -m 5000000 -F 8000000
 gtkwave sega_sound.gtkw
